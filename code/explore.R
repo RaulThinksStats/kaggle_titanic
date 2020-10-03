dec52621fe1dff00bde8e5c66e508a5b3b92377c
RaulThinksStats <torresrj@lakeforest.edu>
2020-10-03 12:59:08 -0500

    more exploring + baseline & refined model

--- code/explore.R
+++ code/explore.R
@@ -18,8 +18,9 @@ train <- read_csv("data/train.csv") %>%
   
 test <- read_csv("data/test.csv") %>% 
   rename_all(tolower)
-preds <- read_csv("data/gender_submission.csv")
-
+preds <- read_csv("data/gender_submission.csv") %>% 
+  rename_all(tolower) %>% 
+  right_join(test)
 
 
 #***********************************************************
@@ -81,6 +82,24 @@ temp <- glm(survived ~ . + sex*pclass, data = train_red, family = "binomial")
 summary(temp)
 predict(temp, test) %>% log()
 
+#explore the third class of female
+female <- train_red %>% 
+  filter(sex == "female", pclass == 3)
+
+female %>% 
+  group_by(survived, pclass, sex) %>% 
+  summarise(fn = n(), mean = mean(age, na.rm = T), sibsp = mean(sibsp), parch = mean(parch))
+
+female %>% 
+  count(survived, sibsp, parch)
+female %>% 
+  count(survived, sibsp)
+female %>% 
+  count(survived, parch)
+
+female %>% 
+  ggplot(aes(x = age)) + geom_histogram() + facet_grid(~survived + sex + pclass) + theme_bw()
+
 #surviving 3rd class males and young people are two other interesting subgroups
 
 #***********************************************************
@@ -91,4 +110,26 @@ predict(temp, test) %>% log()
 
 
 
+#***********************************************************
+# submissions ----
+#***********************************************************
 
+#.75666
+baseline <- read_csv("data/gender_submission.csv") 
+
+#female class 1, 2 , and class 3 rule (all this changed was 26 obs; very little change)
+set.seed(8484)
+pred1 <- preds %>% 
+  mutate(survived = case_when(
+    sex == "female" & pclass < 3 ~ 1,
+    sex == "female" & pclass == 3 & sibsp == 0 ~ 1,
+    sex == "female" & pclass == 3 & sibsp > 0 ~ 99,
+    sex == "male" ~ 0,
+    TRUE ~ 2),
+    survived = ifelse(survived == 99, rbinom(26, 1, .5), survived)) %>%  #got 26 by checking
+  select(passengerid, survived) %>% 
+  rename(PassengerId = passengerid, Survived = survived)
+
+write_csv(pred1, "data/pred1.csv")
+
+#given that there are way more male I will focus now on here. 
