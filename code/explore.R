8868b5983ab8b48ac61da2284dccab448a8523a1
RaulThinksStats <torresrj@lakeforest.edu>
2020-10-03 00:10:21 -0500

    a little more exploring

--- code/explore.R
+++ code/explore.R
@@ -16,7 +16,8 @@ library(tidyverse)
 train <- read_csv("data/train.csv") %>% 
   rename_all(tolower) 
   
-test <- read_csv("data/test.csv")
+test <- read_csv("data/test.csv") %>% 
+  rename_all(tolower)
 preds <- read_csv("data/gender_submission.csv")
 
 
@@ -24,6 +25,13 @@ preds <- read_csv("data/gender_submission.csv")
 #***********************************************************
 # data exploration ----
 #***********************************************************
+
+#initial approach is to remove seemingly uninformative column and rows (come back later to gauge lost info);
+#and look for primary univariate relationships; and confirm those and their relationships.  
+#use these
+
+#reducing cols
+train_red <- select(train, -name, -ticket, -cabin, -passengerid)
 
 colnames(train)
 
@@ -37,8 +45,43 @@ train %>%
 
 
 
+temp <- lm(survived ~ ., data = train_red)
+summary(temp)
+map(train_red, unique)
+temp2 <- glm(survived ~ ., data = train_red, family = "binomial") %>% summary()
+
+#primary univariate relationships (pclass, sex, age; also sibsp)
+count(train_red, survived, pclass)
+count(train_red, survived, sex)
+count(train_red, survived, pclass)
+
+#80% males die relative to 25% of females
+train_red %>% 
+  group_by(survived, sex) %>% 
+  summarise(fn = n(), mean = mean(age, na.rm = T), class = mean(pclass), sibsp = mean(sibsp), parch = mean(parch))
+
+train_red %>% 
+  ggplot(aes(x = age)) + geom_histogram() + facet_grid(~survived + sex) + theme_bw()
+
+#pclass
+train_red %>% 
+  group_by(survived, pclass) %>% 
+  summarise(fn = n(), mean = mean(age, na.rm = T), sibsp = mean(sibsp), parch = mean(parch))
+
+train_red %>% 
+  group_by(survived, pclass, sex) %>% 
+  summarise(fn = n(), mean = mean(age, na.rm = T), sibsp = mean(sibsp), parch = mean(parch))
 
 
+#primary interactions (females in first or second class "always" survive; four buckets remain)
+train_red %>% 
+  ggplot(aes(x = age)) + geom_histogram() + facet_grid(~survived + sex + pclass) + theme_bw()
+
+temp <- glm(survived ~ . + sex*pclass, data = train_red, family = "binomial")
+summary(temp)
+predict(temp, test) %>% log()
+
+#surviving 3rd class males and young people are two other interesting subgroups
 
 #***********************************************************
 # model building ----
